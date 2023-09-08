86894e48d05405ea00bbba46fee0131446c0ce0f
Nagavishnu-Jayam <120970801+Nagavishnu-Jayam@users.noreply.github.com>
2023-09-08 20:17:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def count_batteries_by_health(present_capacities, rated_capacity):
 
 def test_bucketing_by_health():
     print("Counting batteries by SoH...\n")
-    present_capacities = [115, 118, 80, 95, 91, 77]
+    present_capacities = [115, 118, 69, 95, 91, 77]
     rated_capacity = 120
     counts = count_batteries_by_health(present_capacities, rated_capacity)
     assert counts["healthy"] == 2
