68e1959feeb37cd8b2ccaaf3af1d1ddc78fc8f5d
Nagavishnu-Jayam <120970801+Nagavishnu-Jayam@users.noreply.github.com>
2023-09-08 19:33:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def count_batteries_by_health(present_capacities):
-  counts = {
+    counts = {
         "healthy": 0,
         "exchange": 0,
         "failed": 0
     }
-     
+    
     rated_capacity = 120
 
     for i in present_capacities:
@@ -17,16 +17,15 @@ def count_batteries_by_health(present_capacities):
             counts["failed"] += 1
 
     return counts
-  
-def test_bucketing_by_health():
-  print("Counting batteries by SoH...\n")
-  present_capacities = [115, 118, 80, 95, 91, 77]
-  counts = count_batteries_by_health(present_capacities)
-  assert(counts["healthy"] == 2)
-  assert(counts["exchange"] == 3)
-  assert(counts["failed"] == 1)
-  print("Done counting :)")
 
+def test_bucketing_by_health():
+    print("Counting batteries by SoH...\n")
+    present_capacities = [115, 118, 80, 95, 91, 77]
+    counts = count_batteries_by_health(present_capacities)
+    assert counts["healthy"] == 2
+    assert counts["exchange"] == 3
+    assert counts["failed"] == 1
+    print("Done counting :)")
 
 if __name__ == '__main__':
-  test_bucketing_by_health()
+    test_bucketing_by_health()
